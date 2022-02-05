9c406b7a70e79a01689d539441aa2fa4c2d9810d
BisLAnN <huadebislan23@gmail.com>
2022-02-05 14:15:04 +0300

    Добавлены обработчики кнопок

--- BisAppp/Form1.Designer.cs
+++ BisAppp/Form1.Designer.cs
@@ -42,6 +42,7 @@ namespace BisAppp
             this.button1.TabIndex = 0;
             this.button1.Text = "Отобразить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -51,6 +52,7 @@ namespace BisAppp
             this.button2.TabIndex = 1;
             this.button2.Text = "Выход";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
